edb7b5d2b3eb5ade1cbe70c3f585da08910c5b70
Wei Zhao <wei.zhao@rtl-corp.com>
2023-11-14 17:42:10 +1100

    add a xml ex

---
+++ coding/ex3/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+using System.Xml;
+namespace ex3
+{
+	static class Program
+	{
+		static void Main()
+		{
+			XmlDocument doc = new XmlDocument();
+			doc.Load("test2.xml");
+			
+			XmlNamespaceManager nsmgr = new XmlNamespaceManager(doc.NameTable);
+			nsmgr.AddNamespace("df","urn:schemas-microsoft-com:office:spreadsheet");
+			nsmgr.AddNamespace("o","urn:schemas-microsoft-com:office:office");
+			nsmgr.AddNamespace("x","urn:schemas-microsoft-com:office:excel");
+			nsmgr.AddNamespace("dt","uuid:C2F41010-65B3-11d1-A29F-00AA00C14882");
+			nsmgr.AddNamespace("ss","urn:schemas-microsoft-com:office:spreadsheet");
+			nsmgr.AddNamespace("html","http://www.w3.org/TR/REC-html40");
+			XmlNode node = doc.SelectSingleNode("/df:Workbook/df:Worksheet", nsmgr);
+			Console.WriteLine(node.Attributes[0].InnerText);
+			//Console.WriteLine(doc.DocumentElement.ChildNodes[0].ChildNodes[0].Name);
+		
+			
+		}
+	}
+}
